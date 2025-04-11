96a4ba8d1b1dbb884c5c3c0aaab40a9c245149c6
Nishant Kumar Singh <nishant220902@gmail.com>
2025-04-11 18:48:59 +0530

    update the backend url

--- src/services.ts
+++ src/services.ts
@@ -1,1 +1,2 @@
-export const BACKEND_URL = 'http://localhost:5000';
+export const BACKEND_URL = 'https://student-job-tracker-backend-oomn.onrender.com';
+// export const BACKEND_URL = 'http://localhost:5000';
